403ffbcc8fc7fd208751bf99255fc0cec9afaaf2
Brian Donnoe <dunow33@hotmail.com>
2021-10-23 11:40:12 -0400

    having issues getting View Model instantiated, will work on this more later

--- app/src/main/java/com/example/plcurrencyconverter/main/MainViewModel.kt
+++ app/src/main/java/com/example/plcurrencyconverter/main/MainViewModel.kt
@@ -6,12 +6,15 @@ import androidx.lifecycle.viewModelScope
 import com.example.plcurrencyconverter.data.models.Rates
 import com.example.plcurrencyconverter.util.DispatcherProvider
 import com.example.plcurrencyconverter.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 import kotlin.math.round
 
-class MainViewModel @ViewModelInject constructor(
+@HiltViewModel
+class MainViewModel @Inject constructor(
     private val repository: MainRepository,
     private val dispatchers: DispatcherProvider
 ) : ViewModel() {
